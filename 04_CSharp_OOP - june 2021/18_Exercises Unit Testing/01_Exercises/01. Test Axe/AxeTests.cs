1282cdf2b669fbc3858ef46f24616f9f7523739e
VKalenski <vilislavkalenski@gmail.com>
2022-04-04 15:28:49 +0300

    Update files

---
+++ 04_CSharp_OOP - june 2021/18_Exercises Unit Testing/01_Exercises/01. Test Axe/AxeTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+
+[TestFixture]
+public class AxeTests
+{
+    private Axe axe;
+
+    private Dummy dummy;
+
+    [SetUp]
+
+    public void InitializedTest()
+    {
+        axe = new Axe(10, 2);
+        dummy = new Dummy(40, 5);
+    }
+
+    [Test]
+    public void AxeLooseOneDurabilityPointAfterAttack()
+    {
+        axe.Attack(dummy);
+        Assert.AreEqual(1, axe.DurabilityPoints, "Axe does not loose 1 durability point after attack");
+    }
+
+    [Test]
+
+    public void AttackWithBrokenAxe()
+    {
+        axe.Attack(dummy);
+        axe.Attack(dummy);
+
+        Assert.That(() => axe.Attack(dummy),
+            Throws.InvalidOperationException.With
+                .Message.EqualTo("Axe is broken."));
+    }
+}
